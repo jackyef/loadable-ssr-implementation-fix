b59775e7b59abac8692eb27c272e187201c02664
vashchukmaksim <vashchukmaksim@gmail.com>
2018-10-09 11:32:40 +0300

    Yandex Metrika added

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router } from 'react-router-dom';
 import Loadable from 'react-loadable';
+import { YMInitializer } from 'react-yandex-metrika';
 
 import { appContainerHTMLTag } from './config';
 import Routes, { history } from './routes';
@@ -10,9 +11,20 @@ import { renderRoutes } from '@tg/ui/utils';
 
 Loadable.preloadReady().then(() => {
 	ReactDOM.hydrate(
-		<Router history={ history }>
-			{ renderRoutes(Routes) }
-		</Router>,
+		<>
+
+			{/* Yandex Metrika */}
+			{
+				process.env.YANDEX_METRIKA_ACCOUNT
+					? <YMInitializer accounts={[parseInt(process.env.YANDEX_METRIKA_ACCOUNT, 10)]} options={{defer: true}} />
+					: null
+			}
+
+			{/* App */}
+			<Router history={ history }>
+				{ renderRoutes(Routes) }
+			</Router>
+		</>,
 		document.getElementById(appContainerHTMLTag)
 	);
 });
--- webpack.base.js
+++ webpack.base.js
@@ -125,7 +125,8 @@ module.exports = function genConfig(mode) {
 			new webpack.DefinePlugin({
 				'process.env':{
 					'NODE_ENV': JSON.stringify(mode),
-					'SENTRY_DSN_KEY': process.env.SENTRY_DSN_KEY
+					'SENTRY_DSN_KEY': process.env.SENTRY_DSN_KEY,
+					'YANDEX_METRIKA_ACCOUNT': process.env.YANDEX_METRIKA_ACCOUNT
 				}
 			})
 		]
