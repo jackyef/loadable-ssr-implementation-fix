24917148e7f290b0f97d1d141470ed4db28f1363
vashchukmaksim <vashchukmaksim@gmail.com>
2018-12-16 00:01:54 +0300

    refactoring

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
-import { baseAPIUrl } from '@tg/ui/config';
+import { BASE_API_URL } from '@tg/ui/config';
 
 /**
  * App axios instance
  */
 export const axiosInstance = axios.create({
-	baseURL: baseAPIUrl
+	baseURL: BASE_API_URL
 });
