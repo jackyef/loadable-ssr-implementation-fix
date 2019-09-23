32d8974e4b129f2b26b553f444a0e6b4b6242d20
vashchukmaksim <vashchukmaksim@gmail.com>
2019-09-23 16:31:32 +0300

    packages updated + configuration fixes

---
+++ typings/react-transition-group-additional.d.ts
@@ -0,0 +1,11 @@
+export interface ICSSTransitionClassNames {
+	appear?: string;
+	appearActive?: string;
+	appearDone?: string;
+	enter?: string;
+	enterActive?: string;
+	enterDone?: string;
+	exit?: string;
+	exitActive?: string;
+	exitDone?: string;
+}
