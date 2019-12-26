a2463b71f4f4a66db39a8099c5c31649796b0f63
vashchukmaksim <vashchukmaksim@gmail.com>
2019-12-26 20:03:59 +0300

    Landing scroll fixed for native scroller

--- src/utils/nav.ts
+++ src/utils/nav.ts
@@ -13,7 +13,15 @@ export const commonNavBtnProps = (title: string, nav: boolean = false) => {
  * Scroll to
  */
 export const scroll = (scroller: any, ref?: RefObject<any>, offset: number = 0) => {
-	scroller.scrollerElement.scrollTo({
+
+	// Check if we use native scroll or custom
+	const scrollEl = scroller.scrollerElement.classList.contains('native')
+		? document.scrollingElement || document.documentElement
+		: scroller.scrollerElement
+	;
+
+	// Scroll to block
+	scrollEl.scrollTo({
 		behavior: 'smooth',
 		left: 0,
 		top: ref ? ref.current.offsetTop + offset : 0
