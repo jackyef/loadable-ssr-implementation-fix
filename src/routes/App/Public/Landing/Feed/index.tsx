244ae122a199d87d29ea830be90e4789ba1c20af
vashchukmaksim <vashchukmaksim@gmail.com>
2020-05-03 01:47:46 +0300

    clean

--- src/routes/App/Public/Landing/Feed/index.tsx
+++ src/routes/App/Public/Landing/Feed/index.tsx
@@ -2,14 +2,10 @@
  * Show Feed mechanics and possibilities
  * @module Feed
  */
-import _ from 'lodash';
-import moment from 'moment';
 import React, { useState, Ref } from 'react';
 
-import { uuid } from '@tg/utils';
-import { Text, FeaturesSwitcher, FeaturesSwitcherProps, CLIENT_DATEFORMAT } from '@tg/elm';
-import { DataPost } from '@tg/api-proxy-drafts';
-import { resources, IconArchive, IconPen, IconTime } from '@tg/resources';
+import { Text, FeaturesSwitcher, FeaturesSwitcherProps } from '@tg/elm';
+import { IconArchive, IconPen, IconTime } from '@tg/resources';
 
 import {
 	StyledContentBlock,
@@ -17,22 +13,11 @@ import {
 	StyledWrapper
 } from './_styled';
 
-import importedStyles from './Feed.module.less';
-const styles: Styles = importedStyles;
-
-type Styles = {
-	self?: string;
-	left?: string;
-	right?: string;
-	preview_selected?: string;
-	block_hint?: string;
-};
-
 /**
  * Empty posts store and context
  */
-const storePosts = new DataPost();
-const ContextStores = React.createContext({ posts: storePosts });
+// const storePosts = new DataPost();
+// const ContextStores = React.createContext({ posts: storePosts });
 
 type Props = {
 	active?: number;
@@ -63,46 +48,41 @@ const featuresData: FeaturesSwitcherProps[] = [
 /**
  * Test posts data
  */
-const postsData = [
-	{
-		text: `
-			After Yuan Shikai was installed as the second
-			leader Provisional Great President of the Republic of China...
-		`,
-		ad: true,
-		reactions: true,
-		uuid: uuid(4),
-		tags: ['travel', 'history'],
-		publish_at: moment().add(2, 'days').add(3, 'h').format(CLIENT_DATEFORMAT),
-		channel: { ava_small: resources.icon_user }
-	},
-	{
-		text: `
-			The official cause of death for both was
-			“ligature neck compression,” meaning strangulation by a cord...
-		`,
-		views: 1300,
-		uuid: uuid(4),
-		tags: ['travel', 'history', 'longtruestory'],
-		published_at: moment().subtract(1, 'days').subtract(6, 'h').format(CLIENT_DATEFORMAT),
-		channel: { ava_small: resources.icon_user }
-	},
-	{
-		text: `
-			After Yuan Shikai was installed as the second
-			leader Provisional Great President of the Republic of China...
-		`,
-		reactions: true,
-		views: 2400,
-		uuid: uuid(4),
-		tags: ['travel', 'history'],
-		published_at: moment().subtract(4, 'days').subtract(8, 'h').format(CLIENT_DATEFORMAT),
-		channel: { ava_small: resources.icon_user },
-		// TODO: Change to custom reactions
-		likes: 302,
-		dislikes: 11
-	}
-];
+// const postsData = [
+// 	{
+// 		text: `
+// 			After Yuan Shikai was installed as the second
+// 			leader Provisional Great President of the Republic of China...
+// 		`,
+// 		ad: true,
+// 		reactions: true,
+// 		uuid: uuid(4),
+// 		tags: ['travel', 'history'],
+// 		publish_at: moment().add(2, 'days').add(3, 'h').format(CLIENT_DATEFORMAT)
+// 	},
+// 	{
+// 		text: `
+// 			The official cause of death for both was
+// 			“ligature neck compression,” meaning strangulation by a cord...
+// 		`,
+// 		views: 1300,
+// 		uuid: uuid(4),
+// 		tags: ['travel', 'history', 'longtruestory'],
+// 		published_at: moment().subtract(1, 'days').subtract(6, 'h').format(CLIENT_DATEFORMAT)
+// 	},
+// 	{
+// 		text: `
+// 			After Yuan Shikai was installed as the second
+// 			leader Provisional Great President of the Republic of China...
+// 		`,
+// 		reactions: true,
+// 		views: 2400,
+// 		uuid: uuid(4),
+// 		tags: ['travel', 'history'],
+// 		published_at: moment().subtract(4, 'days').subtract(8, 'h').format(CLIENT_DATEFORMAT),
+// 		channel: { ava_small: resources.icon_user }
+// 	}
+// ];
 
 const defaultProps: Partial<Props> = {
 	active: 1
@@ -120,32 +100,11 @@ export const Feed: React.FC<Props> = React.forwardRef(({ active: _active }, ref)
 	return (
 		<StyledContentBlock ref={ ref } bg="blue_10">
 			<StyledWrapper>
-
-				{/* Left part */}
-				<div className={ styles.left }>
-					<Text size={ 12 } color="blue_100">{ 'Organize' }</Text>
-					<StyledHeading h={ 2 }
-						title="Organise posts and activities from all channels in one feed"
-					/>
-					<FeaturesSwitcher features={ featuresData } active={ active } onSwitch={ setActive } />
-				</div>
-
-				{/* Right part */}
-				<ContextStores.Provider value={ { posts: storePosts } }>
-					<ul className={ styles.right }>
-						{
-							_.map(postsData, (data, index) => (
-								<span key={ index } />
-								// <ListPostsPreview key={ index } wrapper="li" post={ data }
-								// 	onClick={ () => setActive(index + 1) }
-								// 	showEditTooltip={ index === 1 }
-								// 	className={ active === (index + 1) ? styles.preview_selected : '' }
-								// />
-							))
-						}
-					</ul>
-				</ContextStores.Provider>
-
+				<Text size={ 12 } color="blue_100">{ 'Organize' }</Text>
+				<StyledHeading h={ 2 }
+					title="Organise posts and activities from all channels in one feed"
+				/>
+				<FeaturesSwitcher features={ featuresData } active={ active } onSwitch={ setActive } />
 			</StyledWrapper>
 		</StyledContentBlock>
 	);
--- src/routes/App/Public/Landing/Stats/index.tsx
+++ src/routes/App/Public/Landing/Stats/index.tsx
@@ -3,341 +3,60 @@
  * feature with short description and demo
  * @module Stats
  */
-import _ from 'lodash';
-import React, { Ref, useState } from 'react';
-import moment from 'moment';
-// import {
-// 	ResponsiveContainer,
-// 	ComposedChart,
-// 	Line,
-// 	Area,
-// 	XAxis,
-// 	YAxis,
-// 	Tooltip
-// } from 'recharts';
+import React from 'react';
 
-import { Button, Heading, Text, ContentBlock } from '@tg/elm';
-import { IconCal } from '@tg/resources';
+import { Heading, Text, Paragraph, Box, ContentBlock } from '@tg/elm';
 
-// Styles
-import importedStyles from './Stats.module.less';
-const styles: Styles = importedStyles;
+// const dateFormat = 'YYYY-MM-DD';
 
-type Styles = {
-	self?: string;
-	info?: string;
-	small_stat?: string;
-	graph?: string;
-	scale_container?: string;
+// const _data = {
+// 	week: [
+// 		{ x: moment().subtract(6, 'days').format(dateFormat), y: 3800 },
+// 		{ x: moment().subtract(5, 'days').format(dateFormat), y: 4200 },
+// 		{ x: moment().subtract(4, 'days').format(dateFormat), y: 4000 },
+// 		{ x: moment().subtract(3, 'days').format(dateFormat), y: 4300 },
+// 		{ x: moment().subtract(2, 'days').format(dateFormat), y: 4400 },
+// 		{ x: moment().subtract(1, 'days').format(dateFormat), y: 4800 },
+// 		{ x: moment().format(dateFormat), y: 5000 }
+// 	],
 
-	// Graph
-	dot?: string;
-	dot_active?: string;
-	tooltip?: string;
-	tick?: string;
-	delta_positive?: string;
-	delta_negative?: string;
-};
+// 	month: [
+// 		{ x: '2019-07-01', y: 2000 },
+// 		{ x: '2019-07-08', y: 2300 },
+// 		{ x: '2019-07-15', y: 2800 },
+// 		{ x: '2019-07-22', y: 2850 },
+// 		{ x: '2019-07-30', y: 3600 }
+// 	],
 
-const dateFormat = 'YYYY-MM-DD';
-
-const _data = {
-	week: [
-		{ x: moment().subtract(6, 'days').format(dateFormat), y: 3800 },
-		{ x: moment().subtract(5, 'days').format(dateFormat), y: 4200 },
-		{ x: moment().subtract(4, 'days').format(dateFormat), y: 4000 },
-		{ x: moment().subtract(3, 'days').format(dateFormat), y: 4300 },
-		{ x: moment().subtract(2, 'days').format(dateFormat), y: 4400 },
-		{ x: moment().subtract(1, 'days').format(dateFormat), y: 4800 },
-		{ x: moment().format(dateFormat), y: 5000 }
-	],
-
-	month: [
-		{ x: '2019-07-01', y: 2000 },
-		{ x: '2019-07-08', y: 2300 },
-		{ x: '2019-07-15', y: 2800 },
-		{ x: '2019-07-22', y: 2850 },
-		{ x: '2019-07-30', y: 3600 }
-	],
-
-	year: [
-		{ x: '2018-12-01', y: 100 },
-		{ x: '2019-01-01', y: 200 },
-		{ x: '2019-02-01', y: 300 },
-		{ x: '2019-03-01', y: 500 },
-		{ x: '2019-04-01', y: 400 },
-		{ x: '2019-05-01', y: 600 },
-		{ x: '2019-06-01', y: 900 },
-		{ x: '2019-07-01', y: 2000 },
-		{ x: '2019-08-01', y: 4800 }
-	]
-};
-
-/**
- * Format date to readable form
- * @param v Date string to format
- * @returns {string} Reformated date string
- */
-const formatDate = (v: string): string => {
-	return moment(v, dateFormat).format('MMM DD');
-};
-
-/**
- * Dot component props
- */
-type Props = {
-	ref?: Ref<any>;
-};
-
-/**
- * Graph colors to be set in JSX
- * neither in CSS file
- */
-// const colors = {
-// 	line: '#0E35C4',
-// 	dotBg: '#fff'
+// 	year: [
+// 		{ x: '2018-12-01', y: 100 },
+// 		{ x: '2019-01-01', y: 200 },
+// 		{ x: '2019-02-01', y: 300 },
+// 		{ x: '2019-03-01', y: 500 },
+// 		{ x: '2019-04-01', y: 400 },
+// 		{ x: '2019-05-01', y: 600 },
+// 		{ x: '2019-06-01', y: 900 },
+// 		{ x: '2019-07-01', y: 2000 },
+// 		{ x: '2019-08-01', y: 4800 }
+// 	]
 // };
 
-// type PropsDot = {
-// 	active?: boolean;
-// 	cx?: number;
-// 	cy?: number;
-// };
-
-/**
- * Dot on a Line graph
- */
-// const Dot: React.FC<PropsDot> = ({ active, cx, cy }) => (
-// 	<circle cx={ cx } cy={ cy } r={ active ? 7 : 5 }
-// 		stroke={ colors.line } strokeWidth={ 2 }
-// 		fill={ active ? colors.line : colors.dotBg }
-// 	/>
-// );
-
-type TickProps = {
-	x?: number;
-	y?: number;
-	payload?: any;
-	formatter?: (v: any) => string;
-	axis?: 'x' | 'y';
-};
-
-/**
- * Custom axis tick (value on axis)
- */
-const AxisTick: React.FC<TickProps> = ({ x, y, payload, formatter, axis }) => {
-	return (
-		<g className={ styles.tick } transform={ `translate(${ x },${ y })` }>
-			<text x={ axis === 'y' ? 25 : 0 } y={ 0 } dy={ axis === 'x' ? 16 : 5 } textAnchor="middle">
-				{ formatter(payload.value) }
-			</text>
-		</g>
-	);
-};
-
-AxisTick.defaultProps = {
-	axis: 'x',
-	formatter: v => v
-};
-
-/**
- * Get previous value (moment date)
- */
-// const getPrev = _.memoize((current: string, scale: 'week' | 'month' | 'year') => {
-
-// 	// Prev value container
-// 	let prev: string = null;
-
-// 	// Choose scale
-// 	switch (scale) {
-// 		case 'week':
-// 			prev = moment(current, dateFormat).subtract(1, 'day').format(dateFormat);
-// 			return _.find(_data.week, v => v.x === prev);
-
-// 		case 'month':
-// 			prev = moment(current, dateFormat).subtract(1, 'week').format(dateFormat);
-// 			return _.find(_data.month, v => v.x === prev);
-
-// 		case 'year':
-// 			prev = moment(current, dateFormat).subtract(1, 'month').format(dateFormat);
-// 			return _.find(_data.year, v => v.x === prev);
-
-// 		default:
-// 			return { x: '', y: 0 };
-// 	}
-// });
-
-// const scaleFrom: any = {
-// 	week: 'day',
-// 	month: 'week',
-// 	year: 'month'
-// };
-
-/**
- * Tooltip
- * @param e Mouse event
- * @param scale Axis scale: year, month, week
- */
-// const TooltipCustom: React.FC<any> = (e: any, scale: any) => {
-// 	if (e.active) {
-
-// 		// Values
-// 		const xValue = _.get(e.payload, '1.payload.x');
-// 		const yValue = _.get(e.payload, '1.payload.y');
-
-// 		// Prev day
-// 		const prevPair = getPrev(xValue, scale);
-// 		const delta = prevPair && (yValue - prevPair.y);
-
-// 		// Render
-// 		return (
-// 			<div className={ styles.tooltip }>
-// 				<ul>
-// 					<li><span>{ yValue }</span>{' members'}</li>
-// 					{
-// 						!delta ? null : (
-// 							<li>
-// 								<span
-// 									className={
-// 										delta > 0
-// 											? styles.delta_positive
-// 											: delta < 0
-// 												? styles.delta_negative
-// 												: ''
-// 									}
-// 								>
-// 									{ `${ delta > 0 ? '+' : '' }${ delta }`}
-// 								</span>{` from the previous ${ scaleFrom[scale] }`}
-// 							</li>
-// 						)
-// 					}
-// 				</ul>
-// 			</div>
-// 		);
-// 	}
-
-// 	return null;
-// };
+type Props = { children: any };
 
 /**
  * Component
  */
 export const Stats: React.FC<Props> = React.forwardRef((props, ref) => {
-
-	// Scale state
-	const [scale, setScale] = useState<'week' | 'month' | 'year'>('week');
-	const [data, setData] = useState<Array<{x: string; y: any}>>(_data.week);
-
-	// Render
 	return (
-		<ContentBlock ref={ ref } className={ styles.self }>
-			<div>
-
-				{/* Short description */}
-				<div className={ styles.info }>
-					<Text size={ 12 } color="blue_100">{ 'Analise' }</Text>
-					<Heading h={ 2 } title="Analise what people like and how fast your channel grows" />
-					<p>{
-						'Unleash your creativity, plan projects from all angles, ' +
-						'and create centralized hubs of information to keep everyone in the loop. '
-					}</p>
-
-					{/* Small stat */}
-					<div className={ `${ styles.delta_positive } ${ styles.small_stat }` }>
-						<span>{ '+74' }</span>
-					</div>
-				</div>
-
-				{/* Graph demo */}
-				<div className={ styles.graph }>
-
-					{/* Scale */}
-					<div className={ styles.scale_container }>
-
-						{/* Predefined scales (week, month, year) */}
-						<ul>
-							<li>
-								<Button title="Week"
-									variant="inline"
-									size="small"
-									state={ scale === 'week' ? 'active' : null }
-									onClick={ () => {
-										setScale('week');
-										setData(_data.week);
-									} }
-								/>
-							</li>
-							<li>
-								<Button title="Month"
-									variant="inline"
-									size="small"
-									state={ scale === 'month' ? 'active' : null }
-									onClick={ () => {
-										setScale('month');
-										setData(_data.month);
-									} }
-								/>
-							</li>
-							<li>
-								<Button title="Year"
-									variant="inline"
-									size="small"
-									state={ scale === 'year' ? 'active' : null }
-									onClick={ () => {
-										setScale('year');
-										setData(_data.year);
-									} }
-								/>
-							</li>
-						</ul>
-
-						{/* Date range (calendar trigger) */}
-						<Button variant="secondary"
-							size="small"
-							icon={ <IconCal /> }
-							title={ `${ formatDate(data[0].x) } - ${ formatDate(data[data.length - 1].x) }` }
-						/>
-					</div>
-
-					{/* Chart */}
-					{/*
-					<ResponsiveContainer height={ 400 } width="100%">
-						<ComposedChart data={ data } margin={ { top: 10, right: 10, bottom: 10, left: 10 } }>
-
-							<defs>
-								<linearGradient id="areaGradient" x1="0" y1="0" x2="0" y2="1">
-									<stop offset="5%" stopColor={ colors.line } stopOpacity={ 0.1 }/>
-									<stop offset="95%" stopColor={ colors.line } stopOpacity={ 0 }/>
-								</linearGradient>
-							</defs>
-
-							<XAxis dataKey="x" scale="point" domain={ ['auto', 'auto'] }
-								tick={ <AxisTick formatter={ v => formatDate(v) }/> }
-								axisLine={ { opacity: 0.2 } } tickLine={ { opacity: 0.2 } }
-							/>
-
-							<YAxis orientation="right" domain={ ['auto', 'auto'] } tick={ <AxisTick axis="y" /> }
-								axisLine={ { opacity: 0.2 } } tickLine={ { opacity: 0.2 } }
-							/>
-
-							<Area dataKey="y" type="monotone" stroke={ null }
-								fillOpacity={ 1 } fill="url(#areaGradient)"
-								dot={ false } activeDot={ false }
-							/>
-
-							<Tooltip cursor={ { opacity: 0.4, strokeWidth: 1, strokeDasharray: '5, 5' }  }
-								content={ (e: any) => TooltipCustom(e, scale) }
-							/>
-
-							<Line dataKey="y" type="monotone" stroke={ colors.line } strokeWidth={ 2 }
-								dot={ <Dot /> } activeDot={ <Dot active /> }
-							/>
-
-						</ComposedChart>
-					</ResponsiveContainer>
-					*/}
-				</div>
-			</div>
+		<ContentBlock ref={ ref } bg="white_100">
+			<Box>
+				<Text size={ 12 } color="blue_100">{ 'Analise' }</Text>
+				<Heading h={ 2 } mb={ 5 } title="Analise what people like and how fast your channel grows" />
+				<Paragraph size={ 18 } color="black_80">{
+					'Unleash your creativity, plan projects from all angles, ' +
+					'and create centralized hubs of information to keep everyone in the loop. '
+				}</Paragraph>
+			</Box>
 		</ContentBlock>
 	);
 });
